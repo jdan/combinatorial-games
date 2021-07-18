745e6fccd41e43f0ba8686655d0b3c685a4aeb97
Jordan Scales <scalesjordan@gmail.com>
2021-07-18 12:54:31 -0400

    add flag game

--- lib/game.ml
+++ lib/game.ml
@@ -1,7 +1,8 @@
 open Util
 
+type result = Win | Lose | Draw
+
 module type CombinatorialGame = sig
-  type result = Win | Lose
   type game_state
 
   val next_states : game_state -> game_state list
@@ -17,16 +18,17 @@ module CombinatorialGameEvaluator (G : CombinatorialGame) = struct
     then Lose
     else
       let eval_next = next_states state |> List.map eval in
-      (* SHAPE: Page 112 *)
+      (* SHAPE: Page 126 *)
       (* If every move I make leads to a W, my current position is an L. *)
       if all (fun r -> r = Win) eval_next
       then Lose
       (* If some move I can make leads to an L, my current position is a W. *)
-      else Win
+      else if any (fun r -> r = Lose) eval_next
+      then Win
+      else Draw
 end
 
 module SinglePileNim = struct
-  type result = Win | Lose
   type game_state = int
 
   let next_states pile = range 0 (pile - 1)
@@ -34,13 +36,12 @@ module SinglePileNim = struct
 end
 
 module SinglePileNimEvaluator = CombinatorialGameEvaluator(SinglePileNim)
-let%test _ = SinglePileNim.Win = SinglePileNimEvaluator.eval 5
-let%test _ = SinglePileNim.Win = SinglePileNimEvaluator.eval 4
-let%test _ = SinglePileNim.Win = SinglePileNimEvaluator.eval 1
-let%test _ = SinglePileNim.Lose = SinglePileNimEvaluator.eval 0
+let%test _ = Win = SinglePileNimEvaluator.eval 5
+let%test _ = Win = SinglePileNimEvaluator.eval 4
+let%test _ = Win = SinglePileNimEvaluator.eval 1
+let%test _ = Lose = SinglePileNimEvaluator.eval 0
 
 module MultiPileNim = struct
-  type result = Win | Lose
   type game_state = int list
 
   let next_states piles =
@@ -58,6 +59,26 @@ module MultiPileNim = struct
 end
 
 module MultiPileNimEvaluator = CombinatorialGameEvaluator(MultiPileNim)
-let%test _ = MultiPileNim.Win = MultiPileNimEvaluator.eval [5]
-let%test _ = MultiPileNim.Lose = MultiPileNimEvaluator.eval [2; 2]
-let%test _ = MultiPileNim.Win = MultiPileNimEvaluator.eval [2; 3]
+let%test _ = Win = MultiPileNimEvaluator.eval [5]
+let%test _ = Lose = MultiPileNimEvaluator.eval [2; 2]
+let%test _ = Win = MultiPileNimEvaluator.eval [2; 3]
+
+module FlagPick = struct
+  type game_state = int
+  (* You can pick 1, 2, or 3 flags *)
+  let next_states n =
+    if n < 4
+    then range 0 (n - 1)
+    else [n - 1; n - 2; n - 3]
+  let is_loss = (=) 0
+end
+
+module FlagPickEvaluator = CombinatorialGameEvaluator(FlagPick)
+let%test _ = Win = FlagPickEvaluator.eval 5
+let%test _ = Win = FlagPickEvaluator.eval 3
+let%test _ = Win = FlagPickEvaluator.eval 2
+let%test _ = Win = FlagPickEvaluator.eval 1
+
+let%test _ = Lose = FlagPickEvaluator.eval 0
+let%test _ = Lose = FlagPickEvaluator.eval 4
+let%test _ = Lose = FlagPickEvaluator.eval 8
--- lib/util.ml
+++ lib/util.ml
@@ -6,6 +6,10 @@ let rec all pred = function
   | [] -> true
   | x :: rest -> if pred x then all pred rest else false
 
+let rec any pred = function
+  | [] -> false
+  | x :: rest -> if pred x then true else any pred rest
+
 let rec update idx v = function
   | [] -> []
   | x::xs -> if idx = 0 then (v :: xs) else x :: update (idx - 1) v xs
